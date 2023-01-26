79cefc72104e608fc92f4d9b2514203ce0be8e41
GeneracChenruWu <cwu@generacgs.com>
2023-01-26 14:20:40 -0800

    Two tests over logging

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TestAndPractice
 For testing and validating and practicing
+
+
+# Logging
+
+tip1: 
+The call to basicConfig() should come before any calls to debug(), info(), etc. Otherwise, those functions will call basicConfig() for you with the default options. As it’s intended as a one-off simple configuration facility, only the first call will actually do anything: subsequent calls are effectively no-ops.
--- testLogging.py
+++ testLogging.py
@@ -1,2 +1,26 @@
 import logging
 
+# test1: if run warning and info first then call basicConfig
+# it will still be the default level, the basicConfig make no
+# effects here
+
+# logging.warning("I told you so")
+# logging.info("this is a information from logging")
+
+# print("----------------")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+)
+
+logging.warning("I told you so")
+logging.info("this is a information from logging")
+
+
+# test2 logging in file
+# import logging
+# logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+# logging.debug('This message should go to the log file')
+# logging.info('So should this')
+# logging.warning('And this, too')
+# logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
